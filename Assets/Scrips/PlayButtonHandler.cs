d94ea88c926bf6c2607fbd93292e3192c2a8a13b
Radeah <113103602+Radeah@users.noreply.github.com>
2024-05-15 17:04:42 +0100

    HOW THE FRICK DOES A PLAY BUTTON NOT WORK

--- Assets/Scrips/PlayButtonHandler.cs
+++ Assets/Scrips/PlayButtonHandler.cs
@@ -1,28 +1,28 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayButtonHandler : MonoBehaviour
 {
-    public string targetButtonTag = "PlayButton"; // Tag assigned to the play button
-
-    public void OnButtonClicked()
+    public void PlayGame()
     {
-        // Check if the clicked button has the correct tag
-        if (gameObject.CompareTag(targetButtonTag))
-        {
-            // Load the next scene
-            LoadNextScene();
-        }
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex * 1);
     }
 
-    // Load the next scene in the build settings
-    private void LoadNextScene()
+    public void QuitGame()
     {
-        // Get the current scene index
-        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-
-        // Load the next scene by incrementing the current scene index
-        SceneManager.LoadScene(currentSceneIndex + 1);
+        Application.Quit();
     }
+    
+
+
+
+
+
+
+
+    
 }
 
