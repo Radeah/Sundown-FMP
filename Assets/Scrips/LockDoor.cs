4a7f88e24ed2ad1d05d4f8947c498637c58122fe
Radeah <113103602+Radeah@users.noreply.github.com>
2024-05-21 15:08:40 +0100

    fixing

--- Assets/Scrips/LockDoor.cs
+++ Assets/Scrips/LockDoor.cs
@@ -4,15 +4,67 @@ using UnityEngine;
 
 public class LockDoor : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject pickUpText; // Reference to the UI text for interaction
+    public GameObject keyItem; // Reference to the key item required to unlock the door
+
+    private bool isLocked = true; // Flag to track whether the door is locked or unlocked
+    private bool isPlayerInTrigger = false; // Flag to track whether the player is in the collider
+
     void Start()
     {
-        
+        SetPickupTextActive(false); // Ensure the text is initially hidden
     }
 
-    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && isLocked)
+        {
+            isPlayerInTrigger = true;
+            if (InventoryManager.instance.HasItem(keyItem))
+            {
+                SetPickupTextActive(true); // Show the interaction text to unlock the door
+            }
+            else
+            {
+                SetPickupTextActive(true); // Show the interaction text that the door is locked
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInTrigger = false;
+            SetPickupTextActive(false); // Hide the interaction text
+        }
+    }
+
     void Update()
     {
-        
+        if (isLocked && isPlayerInTrigger && Input.GetKeyDown(KeyCode.E) && InventoryManager.instance.HasItem(keyItem))
+        {
+            UnlockDoor(); // Unlock the door if the player has the key and presses E
+        }
+    }
+
+    void UnlockDoor()
+    {
+        isLocked = false;
+        SetPickupTextActive(false); // Hide the interaction text
+        Destroy(gameObject); // Destroy the lock object itself
+    }
+
+    void SetPickupTextActive(bool isActive)
+    {
+        pickUpText.SetActive(isActive);
+        if (isActive)
+        {
+            // Set the text to display "Press E to unlock"
+            pickUpText.GetComponent<TextMesh>().text = "Press E to unlock";
+        }
     }
 }
+
+
+
